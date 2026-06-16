f779e92a5d6c916ba6b75171a536bf3dc5502ca8
Shreya Gulia <shreya10109@gmail.com>
2026-06-16 11:45:49 +0530

    update CORS with netlify

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -33,7 +33,7 @@ app.use(cors({
   origin: ['http://127.0.0.1:5500',
     'http://localhost:5500',
     'https://mindora-backend-4m3a.onrender.com',
-    'https://your-site.netlify.app'],
+    'https://mind0ra.netlify.app'],
   credentials: true
 }));
 
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -11,8 +11,9 @@ const server = http.createServer(app);
 // Attach Socket.io
 const io = new Server(server, {
   cors: {
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500',
-      'https://your-site.netlify.app'
+    origin: ['http://127.0.0.1:5500',
+      'http://localhost:5500',
+      'https://mind0ra.netlify.app'
     ],
     methods: ['GET', 'POST']
   }
